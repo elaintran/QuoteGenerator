a5445f84fa475e177cddb2f66a93864bcd6d79ea
elaintran <elain.ttran@gmail.com>
2018-12-18 22:12:41 -0600

    added random colors to quote machine

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var text = document.querySelector("#text");
 var author = document.querySelector("#author");
+var next = document.querySelector("#new-quote");
 
 function getQuotes() {
 	var quoteAPI = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?";
@@ -8,9 +9,19 @@ function getQuotes() {
 		author.textContent = data["0"].title;
 		text.innerHTML = data["0"].content;
 	})
+}
 
+function nextQuote() {
+	getQuotes();
+	getColor();
 }
 
 getQuotes();
 
-var colors = ["#5ec0bc", "#ffc95e", "#f36d6a", "#76a6ef", "#b783c9"];
+var colors = ["#5ec0bc", "#ffc95e", "#f36d6a", "#5ab7ed", "#b783c9"];
+
+function getColor() {
+	var colorNumber = Math.floor(Math.random() * colors.length);
+	var newColor = colors[colorNumber];
+	author.style.backgroundColor = newColor;
+}
