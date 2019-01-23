e8e19573ead9b66398637b3371e5809b0c61ef9c
elaintran <elain.ttran@gmail.com>
2019-01-22 18:47:37 -0600

    added fb share

--- script.js
+++ script.js
@@ -4,57 +4,37 @@ var next = document.querySelector("#new-quote");
 var quoteContainer = document.querySelector("#quote-box");
 var bookmark = document.querySelector("#bookmark");
 
-var authorQuote;
-var textQuote;
+var authorText;
+var quoteText;
 
 function getQuotes() {
 	var quoteAPI = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?";
 	$.getJSON(quoteAPI, function(data) {
 		console.log(data);
-		if (data["0"].content.length >= 260) {
-			getQuotes();
-			authorQuote = data["0"].title;
-			textQuote = data["0"].content;
-			//author.textContent = data["0"].title;
-			//text.innerHTML = data["0"].content;
-		} else if (data["0"].content.length < 260) {
-			//author.textContent = data["0"].title;
-			//text.innerHTML = data["0"].content;
-			authorQuote = data["0"].title;
-			textQuote = data["0"].content;
-		}
-		if (author.textContent === "") {
-			author.textContent = data["0"].title;;
-			text.innerHTML = data["0"].content;
-		}
+		//authorText = data["0"].title;
+		//quoteText = data["0"].content;
+		author.textContent = data["0"].title;
+		text.innerHTML = data["0"].content;
 	})
 }
 
-function returnQuote() {
-	getQuotes();
-	return authorQuote;
-	return textQuote;
-}
-returnQuote();
-
-/*function initialQuote() {
-	getQuotes();
-	author.textContent = authorQuote;
-	text.innerHTML = textQuote;
-}
-initialQuote();*/
+getQuotes();
 
 function nextQuote() {
 	$(".quote-container").animate({ opacity: 0 }, 500, function() {
 	    $(this).animate({ opacity: 1}, 500);
 	    getColor();
 	    getQuotes();
-	    author.textContent = authorQuote;
-	    text.innerHTML = textQuote;
     }
   );
 }
 //nextQuote();
+
+/*$(document).ready(function() {
+	getQuotes().then(() => {
+    	nextQuote();
+  });
+})*/
 
 var colors = ["#5ec0bc", "#ffc95e", "#f36d6a", "#5ab7ed", "#b783c9", "#7ccd83"];
 
@@ -70,6 +50,24 @@ function tweetQuote() {
 	var twitterURL = "https://twitter.com/intent/tweet?text=" + quoteContent + " -" + authorContent;
 	window.open(twitterURL);
 }
+
+function shareFB() {
+	FB.ui({
+	    method: 'share',
+	    display: 'popup',
+	    href: 'https://elaintran.github.io/QuoteGenerator/',
+	},
+  	// callback
+  	function(response) {
+	    if (response && !response.error_message) {
+	    	alert('Posting completed.');
+	    } else {
+	    	alert('Error while posting.');
+	    }
+	})
+}
+
+
 
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
